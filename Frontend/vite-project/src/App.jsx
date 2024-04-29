a138333157181b9e03631da76b113306eff4e9bb
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2024-04-29 14:08:48 +0200

    Siljak frontenda

---
+++ Frontend/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import NavBar from "./components/NavBar";
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+
+   </>
+  )
+}
+
+export default App
