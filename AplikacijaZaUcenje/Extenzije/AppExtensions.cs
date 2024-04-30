34679ac6521c2783829910e0bfde700f8c75aa32
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2024-04-30 19:07:13 +0200

    CORS

---
+++ AplikacijaZaUcenje/Extenzije/AppExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AplikacijaZaUcenje.Extenzije
+{
+    public class AppExtensions
+    {
+        public static void AddAplikacijaCORS(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder =>
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+        }
+    }
+}
--- AplikacijaZaUcenje/Program.cs
+++ AplikacijaZaUcenje/Program.cs
@@ -1,4 +1,5 @@
 using AplikacijaZaUcenje.DATA;
+using AplikacijaZaUcenje.Extenzije;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -11,6 +12,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAplikacijaCORS();
 
 builder.Services.AddDbContext<AplikacijaContext>(AppCont =>
 AppCont.UseSqlServer(builder.Configuration.GetConnectionString(name: "AplikacijaContext")));
